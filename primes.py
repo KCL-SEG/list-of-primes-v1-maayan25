c800e8e54b39b4780ffc156a0a2f6e675dc158e8
Ma'ayan Armony <maayan.armony@kcl.ac.uk>
2022-09-29 21:16:13 +0100

    Add solution to primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    number = 2 
+    while count < number_of_primes:
+        divisible = False
+        for i in range(2, number):
+            if number % i == 0:
+                break
+        else:
+            list.append(number)
+            count += 1
+        number += 1
     return list
